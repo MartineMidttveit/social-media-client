d49d4872f3be2457eed86fc8f8aa4d1797ee342e
Martine Midttveit <martinemidttveit@hotmail.com>
2024-04-13 13:35:46 +0200

    Did some changes in the Cypress test files

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,29 +1,73 @@
-describe('Login Form Validation', () => {
-    it('Displays alert for invalid credentials', () => {
-      cy.visit('http://127.0.0.1:5500'); 
-  
-      cy.get('[data-auth="login"][data-bs-target="#loginModal"]').first().click();
-  
-      cy.get('#loginEmail').type('invalid_email@example.com');
-      cy.get('#loginPassword').type('invalid_password');
-  
-      cy.on('window:alert', (text) => {
-        expect(text).to.contain('Either your username was not found or your password is incorrect');
-      });
-  
-      cy.get('#loginForm').submit();
-    });
-  
-    it('Successfully logs in with valid credentials', () => {
-      cy.visit('http://127.0.0.1:5500'); 
-  
-      cy.get('[data-auth="login"][data-bs-target="#loginModal"]').first().click();
-  
-      cy.get('#loginEmail').type('valid_email@example.no');
-      cy.get('#loginPassword').type('valid_password@example.no');
-  
-      cy.get('#loginForm').submit();
+describe("User login", () => {
+  beforeEach(() => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.wait(1000);
+  });
 
-      cy.get("#logOut").should("exist");
+  context("Valid user credentials", () => {
+    it("Successfully logs in", () => {
+      cy.get("#registerForm").find("button[data-auth=login]").click();
+      cy.wait(700);
+
+      cy.get("#loginForm").within(() => {
+        cy.get('input[name="email"]').type("julius1234@noroff.no");
+        cy.get('input[name="password"]').type("julius123");
+        cy.get('button[type="submit"]').click();
+      });
+
+      cy.wait(1500);
+      cy.url().should("include", "view=profile");
+    });
+
+    it("Stores token in LocalStorage", () => {
+      cy.get("#registerForm").find("button[data-auth=login]").click();
+      cy.wait(700);
+
+      cy.get("#loginForm").within(() => {
+        cy.get('input[name="email"]').type("julius1234@noroff.no");
+        cy.get('input[name="password"]').type("julius123");
+        cy.get('button[type="submit"]').click();
+      });
+
+      cy.wait(1500);
+      cy.window().then((win) => {
+        expect(win.localStorage.getItem("token")).to.exist;
+      });
     });
   });
+
+  context("Invalid user credentials", () => {
+    it("Fails to log in", () => {
+      cy.get("#registerForm").find("button[data-auth=login]").click();
+      cy.wait(700);
+
+      cy.get("#loginForm").within(() => {
+        cy.get('input[name="email"]').type("invalid@email.com");
+        cy.get('input[name="password"]').type("invalidpassword");
+        cy.get('button[type="submit"]').click();
+      });
+
+      cy.wait(1500);
+      cy.on("window:alert", (text) => {
+        expect(text).to.contain(
+          "Either your username was not found or your password is incorrect",
+        );
+      });
+    });
+
+    it("Does not store token in LocalStorage", () => {
+      cy.get("#registerForm").find("button[data-auth=login]").click();
+      cy.wait(700);
+
+      cy.get("#loginForm").within(() => {
+        cy.get('input[name="email"]').type("invalid@email.com");
+        cy.get('input[name="password"]').type("invalidpassword");
+        cy.get('button[type="submit"]').click();
+      });
+      cy.wait(1500);
+      cy.window().then((win) => {
+        expect(win.localStorage.getItem("token")).to.be.null;
+      });
+    });
+  });
+});
